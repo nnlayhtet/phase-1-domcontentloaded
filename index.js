eaca221413f0892d7e8e408b577cbde1f2fd6d10
nyinyilayhtet <nnlayhtet@gmail.com>
2022-05-02 17:14:45 -0400

    commit tree 062a7847d1164468d0811958a95390aaecb6e400

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    let textChange = document.getElementById("text");
+    textChange.textContent = "This is really cool!"
+})
